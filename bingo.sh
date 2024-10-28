c299fe68868d8cd82208256af92b623aeec789c6
Bang Jak <dzakyramadhani14510@gmail.com>
2024-10-28 07:38:31 +0000

    Save progress: 970.1

--- bingo.sh
+++ bingo.sh
@@ -7,8 +7,11 @@ TEXT="The next number is, ";
 if (( NUMBER <= 15 ))
 then
   echo $TEXT B:$NUMBER
-elif [[ $NUMBER -le 30 ]]
+elif [[ NUMBER -le 30 ]]
 then
   echo $TEXT I:$NUMBER
+elif (( NUMBER < 46 ))
+then
+  echo $TEXT N:$NUMBER
 fi
 
