866df0557fc05f21fa9aa2cf8387f570c8dffcb0
Bang Jak <dzakyramadhani14510@gmail.com>
2024-10-28 07:34:47 +0000

    Save progress: 950.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Bingo Number Generator
 echo -e "\n~~ Bingo Number Generator ~~\n"
-NUMBER=$((RANDOM%75+1))
-echo $NUMBER
-TEXT="The next number is, "
+NUMBER=$((RANDOM%15+1))
+TEXT="The next number is, ";
 
-echo $TEXT
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
+fi
+
